358f05f8c7b2770a46271c118565b92e3908f27c
Nwosu Joseph Chuks <chuksdj07@gmail.com>
2023-07-26 23:49:43 +0800

    resizing

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -89,7 +89,7 @@ const MovieList = () => {
        <div key={id} className="transition-all duration-500  ease-in-out hover:scale-110 flex flex-col lg:mb-10  items-center p-2 rounded-lg bg-black shadow-md border-t-[1px] border-gray-700 shadow-gray-700 ">
        
         <img className="lg:w-52 lg:h-60 w-40 h-48"
-         src={poster_path ? `https://image.tmdb.org/t/p/original${poster_path}` : "unavailable"} 
+         src={poster_path ? `https://image.tmdb.org/t/p/w154${poster_path}` : "unavailable"} 
          alt={original_title || title} 
          />
          <div className="flex flex-col text-white w-40 lg:w-52 mt-2">
