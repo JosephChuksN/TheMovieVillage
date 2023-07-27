a8d8356de9b384ec41dcf13e9d7b5e1d598c7e00
Nwosu Joseph Chuks <chuksdj07@gmail.com>
2023-07-27 23:27:11 +0800

    fixed issue

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { createStore } from "redux";
 import reducers from "./reducers";
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' 
-import ReduxThunk from "redux-thunk"
+
 
 
 const persistConfig = {
